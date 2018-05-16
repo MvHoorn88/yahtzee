b79eda5040e2e14899a18a9013fca94e6dce51b0
MvHoorn88 <m.hoorn88@gmail.com>
2018-05-16 17:20:35 +0200

    5 classes

---
+++ yahtzee/Dice.java
@@ -0,0 +1,15 @@
+package yahtzee;
+
+public class Dice {
+
+	Dice dice1 = new Dice();
+	Dice dice2 = new Dice();
+	Dice dice3 = new Dice();
+	Dice dice4 = new Dice();
+	Dice dice5 = new Dice();
+	
+
+
+
+}
+
